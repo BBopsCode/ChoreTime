ee811a49503f6ccb9d8a48efd7737286ad255283
Lemonpathic <124409140+Lemonpathic@users.noreply.github.com>
2024-06-03 16:22:46 -0400

    changed parent folder

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -13,14 +14,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    outDir: 'dist',  // Ensure the build output is in the dist directory
-    rollupOptions: {
-      external: [
-        '/chore-time/src/main.js'  // Add this line to mark the module as external
-      ]
-    }
-  },
-  base: '/chore-time/'  // Set the base to the subdirectory
+  }
 })
